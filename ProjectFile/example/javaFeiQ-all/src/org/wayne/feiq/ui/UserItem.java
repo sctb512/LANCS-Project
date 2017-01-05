99049eee1a48142a78ac57a471f1bbfe1f1f14b6
爱上民谣的少年 <1506376703@qq.com>
2017-01-05 10:17:37 -0500

    no commit message

---
+++ ProjectFile/example/javaFeiQ-all/src/org/wayne/feiq/ui/UserItem.java
@@ -0,0 +1,51 @@
+package org.wayne.feiq.ui;
+
+import java.net.InetAddress;
+
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+import org.wayne.feiq.data.User;
+
+import com.swtdesigner.SWTResourceManager;
+
+public class UserItem extends TableItem {
+
+	public UserItem(Table parent, int style) {
+		super(parent, style);
+	}
+
+	protected User user;
+
+	public UserItem setUser(User user) {
+		this.user = user;
+		InetAddress address = this.user.getAddress();
+		setImage(SWTResourceManager.getImage(UserItem.class,
+				"/org/wayne/feiq/ui/image/photo.small.png"));
+		setText(user.getUserName() + '(' + address.getHostName() + ')'
+				+ address.getHostAddress());
+		return this;
+	}
+
+	public void setChecked(boolean b) {
+		if (b) {
+			setImage(SWTResourceManager.getImage(UserItem.class,
+					"/org/wayne/feiq/ui/image/photo.big.png"));
+		} else {
+			setImage(SWTResourceManager.getImage(UserItem.class,
+					"/org/wayne/feiq/ui/image/photo.small.png"));
+		}
+		super.setChecked(b);
+	}
+
+	public boolean isSameAs(User user) {
+		return user.equals(this.user);
+	}
+
+	public boolean isSameAs(InetAddress address) {
+		return address.equals(user.getAddress());
+	}
+
+	protected void checkSubclass() {
+	}
+
+}
