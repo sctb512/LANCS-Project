a2073b3820ae08317ca9b9611a1cd72b0ee6969a
cwnu-abin <1506376703@qq.com>
2017-01-08 08:33:23 -0500

    no commit message

---
+++ 代码实现/LANCS-LocalAreaNetworkCommunicationSystem/src/com/PacketLANCS/MainFrame.java
@@ -0,0 +1,7 @@
+package com.PacketLANCS;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	//310*510
+}
---
+++ 代码实现/LANCS-LocalAreaNetworkCommunicationSystem/src/com/PacketLANCS/Register.java
@@ -0,0 +1,5 @@
+package com.PacketLANCS;
+
+public class Register {
+
+}
